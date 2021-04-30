7ba1710b4f7ab5438251c6df324c75a1d1298b28
Jason Leitch <jason.leitch@arnoldclark.com>
2021-05-01 00:07:46 +0100

    fixing tf outputs to work with ansible

--- terraform/vms/outputs.tf
+++ terraform/vms/outputs.tf
@@ -1,9 +1,10 @@
-output "instance-name" {
+// understore in output name here is important as it's consumed by ansible.
+output "instance_name" {
   value = module.azure-pipeline-provision-iweb-instance.instance-name
-  description   = "unique name of a vm created with molecule create, used to populate ansible inventory file"
+  description   = "unique name of a vm created with 'molecule create', used to populate ansible inventory file"
 }
 
-output "instance-ip" {
+output "instance_ip" {
   value = module.azure-pipeline-provision-iweb-instance.instance-ip
-  description   = "unique ip of a vm created with molecule create, used to populate ansible inventory file"
+  description   = "unique ip of a vm created with 'molecule create', used to populate ansible inventory file"
 }
