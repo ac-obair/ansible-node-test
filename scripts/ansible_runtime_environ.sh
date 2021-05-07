345ff4e547fc034faa169a6f2b2008407d941320
Jason Leitch <jason.leitch@arnoldclark.com>
2021-05-07 13:13:14 +0100

    outputting vars

--- scripts/ansible_runtime_environ.sh
+++ scripts/ansible_runtime_environ.sh
@@ -5,6 +5,11 @@ REPO_NAME=$2
 SHORT_BRANCH_NAME=$(basename ${COMPLETE_BRANCH_NAME})
 ENVIRON=${REPO_NAME}-${SHORT_BRANCH_NAME}
 
+echo branch name ${COMPLETE_BRANCH_NAME}
+echo repo name ${REPO_NAME}
+echo branch name short ${SHORT_BRANCH_NAME}
+echo environ ${ENVIRON}
+
 pyenv virtualenvs --bare | grep ${ENVIRON} && pyenv virtualenv-delete -f ${ENVIRON} || echo Creating venv for the first time.
 PYENV_VERSION=3.9.2 pyenv virtualenv -f ${ENVIRON}
 cd $HOME/azure_pipelines/${REPO_NAME} && pyenv local ${ENVIRON}
