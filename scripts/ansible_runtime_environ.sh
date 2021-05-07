042777e7b21d7e84feb580cb4cb5001ae45e3338
Jason Leitch <jason.leitch@arnoldclark.com>
2021-05-07 13:35:57 +0100

    adding comments

--- scripts/ansible_runtime_environ.sh
+++ scripts/ansible_runtime_environ.sh
@@ -1,5 +1,9 @@
 #!/usr/bin/env bash
 
+# args to this script are passed from ansible_runtime_environ.yaml as args: "$(System.PullRequest.SourceBranch) ${{ parameters.pipeline }}"
+# if this script breaks with python 2.7 error, it's due a broken link and the env must be manually cleaned up on the ACS. 
+# to resolve this simply rm -rf $HOME/.pyenv/versions/<YOUR_PYTHON_VERSION>/envs/<ENVIRON>
+
 COMPLETE_BRANCH_NAME=$1
 REPO_NAME=$2
 SHORT_BRANCH_NAME=$(basename ${COMPLETE_BRANCH_NAME})
